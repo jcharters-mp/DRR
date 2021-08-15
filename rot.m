0b3acbd039f2f134deace140ef58e0115026ca27
John Charters <59131922+jcharters-mp@users.noreply.github.com>
2021-08-14 18:16:06 -0700

    Update rot.m

--- rot.m
+++ rot.m
@@ -1,22 +1,29 @@
 %% Rodrigues' rotation formula
-% John Charters
-% Fall 2020
+% John Charters, M.S.
+% David Geffen School of Medicine at UCLA
 
-% Goal: build rotation matrix about an arbitrary axis
+%{
+This function builds a rotation matrix about an arbitrary axis
 
-% Reference: "Rodrigues' rotation formula" (Wikipedia)
+Reference
+"Rodrigues' rotation formula" - Wikipedia
 
-% Input --
-% (kx,ky,kz): unit vector rotation axis
-% theta: rotation angle (+ve = ccw, -ve = cw) [rad]
+Input
+(kx,ky,kz): unit vector rotation axis
+theta: rotation angle (+ve = ccw, -ve = cw) [rad]
 
-% Output --
-% R: rotation matrix, so that v_rot = R*v
+Output
+R: rotation matrix, so that v_rot = R*v
+%}
 
 function R = rot(k,theta)
 
-if norm(k) ~= 1
-    error('k needs to be normalized');
+if norm(k)==0
+    error('axis cannot be zero');
+end
+
+if norm(k)~=1
+    k = k / norm(k);
 end
 
 matK = [0 -k(3) k(2); k(3) 0 -k(1); -k(2) k(1) 0];
@@ -24,4 +31,3 @@ matK = [0 -k(3) k(2); k(3) 0 -k(1); -k(2) k(1) 0];
 R = eye(3) + sin(theta)*matK + (1-cos(theta))*matK*matK;
 
 end
-
