e87608082633b4252803a4c9fc7ee4ed6bb8fa54
John Charters <59131922+jcharters-mp@users.noreply.github.com>
2021-08-14 17:05:54 -0700

    Update ReadDicom.cxx
    follow Apache 2.0 license guidelines

--- ITK/ReadDicom.cxx
+++ ITK/ReadDicom.cxx
@@ -1,3 +1,30 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+// Read a CT volume in DICOM format and export to an nrrd file
+// This function is an edited version of ITK's DicomSeriesReadImageWrite2.cxx
+// John Charters
+//
+// Apache 2.0 license guidelines:
+// License and Notice are provided together with this function
+// List of changes made to DicomSeriesReadImageWrite2.cxx
+// - many original comments removed for brevity
+
 #include "itkImage.h"
 #include "itkGDCMImageIO.h"
 #include "itkGDCMSeriesFileNames.h"
